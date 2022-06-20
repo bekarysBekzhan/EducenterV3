17a6ba5b13b4ba9fd58cde627cf14df19486c31e
Ruslan-BG <praktika.95@gmail.com>
2022-06-20 17:45:58 +0600

    Create SimpleButton.js

---
+++ src/components/button/SimpleButton.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const SimpleButton = () => {
+  return (
+    <View>
+      <Text>SimpleButton</Text>
+    </View>
+  );
+};
+
+export default SimpleButton;
