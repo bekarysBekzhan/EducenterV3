af4da3b8c23de4ecae3b3ebb132e755005784264
Ruslan-BG <praktika.95@gmail.com>
2022-06-23 10:30:57 +0600

    add setFontStyle

---
+++ src/utils/setFontStyle.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+import {ColorApp} from '../constans/ColorApp';
+
+export const setFontStyle = (
+  fontSize = 16,
+  fontWeight = '400',
+  color = ColorApp.font,
+  lineHeight = fontSize + 3
+) => {
+  const styles = StyleSheet.create({
+    font: {
+      fontSize,
+      fontWeight,
+      color,
+      lineHeight
+    }
+  });
+
+  return styles.font;
+};
