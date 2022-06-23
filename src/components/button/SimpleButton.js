ae1a325a2b62a3c19f1c8ee6734b9f2ba9711050
Ruslan-BG <praktika.95@gmail.com>
2022-06-23 10:49:56 +0600

    done SimpleButton.js

--- src/components/button/SimpleButton.js
+++ src/components/button/SimpleButton.js
@@ -1,12 +1,20 @@
 import React, {useMemo} from 'react';
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import {ColorApp} from '../../constans/ColorApp';
+import {setFontStyle} from '../../utils/setFontStyle';
 
 const SimpleButton = ({
-  onPress = null,
+  onPress = () => undefined,
   loading = false,
   style,
   text,
   textStyle,
+  ...buttonProps
 }) => {
   const memoStyle = useMemo(() => [styles.button, style], [style]);
   const memoTextStyle = useMemo(() => [styles.text, textStyle], [textStyle]);
@@ -16,8 +24,15 @@ const SimpleButton = ({
       style={memoStyle}
       activeOpacity={0.9}
       onPress={onPress}
-      disabled={loading}>
-      <Text style={memoTextStyle}>{text}</Text>
+      disabled={loading}
+      {...buttonProps}>
+      {loading ? (
+        <ActivityIndicator color={'#fff'} />
+      ) : (
+        <Text style={memoTextStyle} numberOfLines={2}>
+          {text}
+        </Text>
+      )}
     </TouchableOpacity>
   );
 };
@@ -26,7 +41,16 @@ export default SimpleButton;
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '',
+    backgroundColor: ColorApp.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 14,
+    paddingHorizontal: 16,
+    maxHeight: 50,
+    borderRadius: 10,
   },
-  text: {},
+  text: {
+    ...setFontStyle(17, '600', '#fff'),
+    textAlign: 'center',
+  },
 });
