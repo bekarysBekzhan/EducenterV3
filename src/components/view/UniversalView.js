4f540f33e31d5f5a078bc0894f19505e3ce5a5c6
Ruslan-BG <praktika.95@gmail.com>
2022-07-05 13:36:38 +0600

    change unversalView

--- src/components/view/UniversalView.js
+++ src/components/view/UniversalView.js
@@ -7,17 +7,25 @@ import {
 } from 'react-native';
 import React, {useMemo} from 'react';
 import Loader from '../Loader';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const UniversalView = ({
   children,
   style,
   haveLoader = false,
   haveSafe = false,
+  haveScroll = false,
+  ...scrollProps
 }) => {
   const memoStyle = useMemo(() => {
     if (haveSafe) {
       return [styles.safeView, style];
     }
+
+    if (haveScroll) {
+      return [styles.scrollView, style];
+    }
+
     return [styles.view, style];
   }, [style]);
 
@@ -30,6 +38,14 @@ const UniversalView = ({
       );
     }
 
+    if (haveScroll) {
+      return (
+        <KeyboardAwareScrollView style={memoStyle} {...scrollProps}>
+          <Loader />
+        </KeyboardAwareScrollView>
+      );
+    }
+
     return (
       <View style={memoStyle}>
         <Loader />
@@ -39,6 +55,14 @@ const UniversalView = ({
 
   if (haveSafe) {
     return <SafeAreaView style={memoStyle}>{children}</SafeAreaView>;
+  }
+
+  if (haveScroll) {
+    return (
+      <KeyboardAwareScrollView style={memoStyle} {...scrollProps}>
+        {children}
+      </KeyboardAwareScrollView>
+    );
   }
 
   return <View style={memoStyle}>{children}</View>;
@@ -55,5 +79,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: Platform.OS == 'ios' ? 0 : StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: '#fff',
   },
 });
