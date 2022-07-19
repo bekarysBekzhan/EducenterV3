bb2baba1ad4f547bfa664a819f9694692f87953c
YeldosQoja <yeldos.kozha@gmail.com>
2022-07-19 14:10:51 +0600

    Created AnswerOption.js

---
+++ src/components/test/AnswerOption.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AnswerOption = () => {
+  return (
+    <View>
+      <Text>AnswerOption</Text>
+    </View>
+  )
+}
+
+export default AnswerOption
