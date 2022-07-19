46b1e3c085da225c35298754ca8b0a653e2674e7
YeldosQoja <yeldos.kozha@gmail.com>
2022-07-19 16:04:59 +0600

    Created Question.js

---
+++ src/components/test/Question.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Question = ({item, index}) => {
+  return (
+    <View>
+      <Text>Question</Text>
+    </View>
+  )
+}
+
+export default Question
