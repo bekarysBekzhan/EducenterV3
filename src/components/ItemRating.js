13ea025c2d22bcaff427a231b596c7321c7c4ecd
YeldosQoja <yeldos.kozha@gmail.com>
2022-07-20 11:49:05 +0600

    Created ItemRating component #44

---
+++ src/components/ItemRating.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ItemRating = ({rating, reviewCount}) => {
+  return (
+    <View>
+      <Text>ItemRating</Text>
+    </View>
+  )
+}
+
+export default ItemRating
