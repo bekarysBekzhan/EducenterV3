e9657ac1f22cfa8e35276cf541550589c8736daf
YeldosQoja <yeldos.kozha@gmail.com>
2022-07-28 15:36:57 +0600

    Inserted AnswerOption into Question #16

--- src/components/test/Question.js
+++ src/components/test/Question.js
@@ -1,42 +1,63 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import AudioPlayer from '../AudioPlayer'
 import MathView from './MathView'
-import { setFontStyle } from '../../utils/utils'
+import { selectComponent, setFontStyle } from '../../utils/utils'
 import { ColorApp } from '../../constans/constants'
 import HtmlView from '../HtmlView'
 import { getAudioUrl } from '../../utils/regex'
-import SimpleButton from '../button/SimpleButton'
-import TrackPlayer from 'react-native-track-player'
+import AnswerOption from './AnswerOption'
 
-const Question = ({item, index, onTrackChange, children}) => {
+const DATA = [
+  {
+    id: 1,
+    answer: "Пользователю легко с интерфейсом"
+  },
+  {
+    id: 2,
+    answer: "<div class=\"ckeditor-html5-audio\" style=\"text-align:center\">\r\n<audio controls=\"controls\" controlslist=\"nodownload\" src=\"https://demo.educenter.kz/storage/files/243573902162d8ffa8da5fa8.96197209_Audio - Steve Jobs - Stay Hung.mp3\">&nbsp;</audio>\r\n</div>\r\n\r\n<p>&nbsp;</p>",
+  },
+  {
+    id: 3,
+    answer: "<p><span class=\"math-tex\">\\(x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}\\)</span></p>"
+  },
+]
+
+const selecteds = [false, true, false]
+
+const Question = ({item, index, onTrackChange, is_multiple, extraStyle, extraTextStyle}) => {
+
+
+  const memoStylesContainer = useMemo(() => [styles.container, extraStyle], [])
+  const memoStylesText = useMemo(() => [setFontStyle(18, "400", ColorApp.primary), {}, extraTextStyle], [])
+  const [selected, setSelected] = useState(null)
 
   useEffect(() => {
-    // console.log('item : ' , item)
+    console.log((index + 1) + " question")
   }, [])
 
   return (
     <View
-      style={styles.container}
+      style={memoStylesContainer}
     >
-      <Text style={[setFontStyle(18, "400", ColorApp.primary)]}>{index+1} - вопрос</Text>
+      <Text style={memoStylesText}>{index+1} - вопрос</Text>
       {
-        item?.question.includes("audio")
-        ?
-        <AudioPlayer url={getAudioUrl(item.question)} _index={index} onTrackChange={onTrackChange}/>
-        :
-        item?.question.includes("math-text")
-        ?
-        <MathView text={item.question}/>
-        :
-        item?.question
-        ?
-        <HtmlView html={item.question} baseStyle={styles.baseStyle} tagsStyles={tagsStyles}/>
-        :
-        null
+        selectComponent(item?.question, 
+          <AudioPlayer url={getAudioUrl(item.question)} _index={index} onTrackChange={onTrackChange}/>,
+          <MathView text={item.question} mathStyle={{padding: 14}}/>,
+          <HtmlView html={item.question} baseStyle={styles.baseStyle} tagsStyles={tagsStyles}/>
+        )
       }
       {
-        children
+        DATA.map((option, index) => (
+          <AnswerOption
+            key={index}
+            selected={selecteds[index]}
+            answer={option.answer}
+            is_multiple={is_multiple}
+            onTrackChange={onTrackChange}
+          />
+        ))
       }
     </View>
   )
@@ -45,7 +66,8 @@ const Question = ({item, index, onTrackChange, children}) => {
 const styles = StyleSheet.create({
   container: {
     alignItems: "flex-start",
-    justifyContent: "flex-start"
+    justifyContent: "flex-start",
+    marginBottom: 20
   },
   baseStyle: {
 
