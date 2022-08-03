be35b6760b95e372cbd4d369c8a84d1eb908efef
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-03 18:06:01 +0600

    Added conditions to enable modules

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { ROUTES } from '../../constans/constants'
+import { APP_COLORS, ROUTES, ROUTE_NAMES } from '../../constans/constants'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { useFetching } from '../hooks/useFetching'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import { MobileSettingsService } from '../../services/API'
 
 const MainStack = createNativeStackNavigator()
 const Navigation = ({
     isAuth = false
 }) => {
-
     return (
       <NavigationContainer>
         <MainStack.Navigator
@@ -25,11 +27,11 @@ const Navigation = ({
             isAuth
             ?
             ROUTES.private.map((route, index) => (
-              <MainStack.Screen name={route.name} component={route.component} initialParams={{ ROUTES: route?.ROUTES }}/>
+              <MainStack.Screen name={route.name} component={route.component} key={index} initialParams={{ ROUTES: route?.ROUTES }}/>
             ))
             :
             ROUTES.public.map((route, index) => (
-              <MainStack.Screen name={route.name} component={route.component} initialParams={{ ROUTES: route?.ROUTES }}/>
+              <MainStack.Screen name={route.name} component={route.component} key={index} initialParams={{ ROUTES: route?.ROUTES }}/>
             ))
           }
         </MainStack.Navigator>
@@ -62,15 +64,67 @@ const BottomTabBar = ({
   route, navigation
 }) => {
 
+  const [data, setData] = useState(null)
+  const [fetchSettings, isLoading, settingsError] = useFetching(async() => {
+    const response  = await MobileSettingsService.fetchSettings()
+    setData(response.data?.data)
+  })
+
+  useEffect(() => {
+    console.log("index.js")
+    fetchSettings()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <View
+        style={styles.container}
+      >
+        <ActivityIndicator color={APP_COLORS.primary}/>
+      </View>
+    )
+  } 
   return(
     <BottomTabStack.Navigator>
       {
-        route?.params?.ROUTES.map((route, index) => (
-          <BottomTabStack.Screen name={route.name} component={route.component} key={index}/>
-        ))
+        route?.params?.ROUTES.map((route, index) => {
+
+          const screen = <BottomTabStack.Screen name={route.name} component={route.component} key={index}/>
+
+          if (route.name === ROUTE_NAMES.coursesStack) {
+            if (data?.modules_enabled_courses) {
+              return screen
+            } else {
+              return null
+            }
+          } else if (route.name === ROUTE_NAMES.testsStack) {
+            if (data?.modules_enabled_tests) {
+              return screen
+            } else {
+              return null
+            }
+          } else if (route.name === ROUTE_NAMES.tasksStack) {
+            if (data?.modules_enabled_tasks) {
+              return screen
+            } else {
+              return null
+            }
+          }
+          
+          return screen
+        })
       }
     </BottomTabStack.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export  {Navigation, SplashNavigation, BottomTabBar}
