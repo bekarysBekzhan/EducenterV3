afce9a6d75a3d35bbd56714107492ad91cdea22e
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-12 14:25:27 +0600

    SelectCategoryScreen #74

--- src/screens/SelectCategoryScreen.js
+++ src/screens/SelectCategoryScreen.js
@@ -1,11 +1,42 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import UniversalView from '../components/view/UniversalView'
+import { FlatList } from 'react-native-gesture-handler'
+import { useState } from 'react'
+import SelectOption from '../components/SelectOption'
 
-const SelectCategoryScreen = () => {
+const SelectCategoryScreen = ({ navigation, route }) => {
+
+  const [currentKey, setCurrentKey] = useState(route.params.category)
+
+  const selectKeyPressed = (value) => {
+    if(currentKey !== value) {
+      setCurrentKey(value)
+      route.params.setCategory(value)
+      route.params.setSelectedCategory(value.name)
+    }
+  }
+
+  const renderItem = ({ item, index }) => {
+    return(
+      <SelectOption
+        value={item}
+        _key={item?.id}
+        label={item?.name}
+        currentKey={currentKey}
+        selectKeyPressed={selectKeyPressed}
+      />
+    )
+  }
+
   return (
-    <View>
-      <Text>SelectCategoryScreen</Text>
-    </View>
+    <UniversalView>
+      <FlatList
+        data={route.params.data}
+        renderItem={renderItem}
+        keyExtractor={(_, index) => index.toString()}
+      />
+    </UniversalView>
   )
 }
 
