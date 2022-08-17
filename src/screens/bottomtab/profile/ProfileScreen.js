73ae1a420c0e6b1a937deb5a9789f3097015d58a
Ruslan-BG <praktika.95@gmail.com>
2022-08-17 12:01:10 +0600

    add icon and profilescreen

--- src/screens/bottomtab/profile/ProfileScreen.js
+++ src/screens/bottomtab/profile/ProfileScreen.js
@@ -1,12 +1,89 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React, {Fragment} from 'react';
+import UniversalView from '../../../components/view/UniversalView';
+import {strings} from '../../../localization';
+import NavButtonRow from '../../../components/view/NavButtonRow';
+import SectionView from '../../../components/view/SectionView';
+import {Calendar, History, Password} from '../../../assets/icons';
 
 const ProfileScreen = () => {
-  return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
-  )
-}
+  const MENU = [
+    {
+      section: strings['Мои профиль'],
+      data: [
+        {
+          id: 1,
+          text: strings['История оплаты'],
+          iconLeft: <History />,
+        },
+        {
+          id: 2,
+          text: 'Расписание',
+          iconLeft: <Calendar />,
+        },
+        {
+          id: 3,
+          text: strings['Сменить пароль'],
+          iconLeft: <Password />,
+        },
+        {
+          id: 4,
+          text: strings.Настройки,
+        },
+      ],
+    },
+    {
+      section: 'Меню',
+      data: [
+        {
+          id: 1,
+          text: 'Журнал',
+        },
+        {
+          id: 2,
+          text: strings.Рейтинг,
+        },
+        {
+          id: 3,
+          text: strings['Реферальная программа'],
+        },
+        {
+          id: 4,
+          text: strings.Новости,
+        },
+        {
+          id: 5,
+          text: strings['Правила и соглашения'],
+        },
+      ],
+    },
+    {
+      section: 'Помощ',
+      data: [
+        {
+          id: 1,
+          text: '',
+        },
+        {
+          id: 2,
+          text: 'О сервисе ',
+        },
+      ],
+    },
+  ];
 
-export default ProfileScreen
+  return (
+    <UniversalView haveLoader={false} haveScroll>
+      {MENU.map(s => (
+        <Fragment>
+          <SectionView label={s.section} />
+          {s.data.map(d => {
+            return <NavButtonRow leftIcon={d.iconLeft} title={d.text} />;
+          })}
+        </Fragment>
+      ))}
+    </UniversalView>
+  );
+};
+
+export default ProfileScreen;
