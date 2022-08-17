27736d47c2022115dc0ea147f1ff35f1fde07c6f
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-17 18:17:06 +0600

    Installed package react-native-file-viewer

--- android/app/src/main/java/com/educenter/MainApplication.java
+++ android/app/src/main/java/com/educenter/MainApplication.java
@@ -1,5 +1,5 @@
 package com.educenter;
-
+import com.vinzscam.reactnativefileviewer.RNFileViewerPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -29,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new RNFSPackage())
+          packages.add(new RNFSPackage());
           return packages;
         }
 
