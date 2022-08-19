464c13cf8cbf28817b21e677030c1eee400e0e35
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-19 16:47:08 +0600

    Created new screen PreviewTestScreen. #87

---
+++ src/screens/PreviewTestScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const PreviewTestScreen = props => {
+  return (
+    <View>
+      <Text>PreviewTestScreen§</Text>
+    </View>
+  );
+};
+
+export default PreviewTestScreen;
