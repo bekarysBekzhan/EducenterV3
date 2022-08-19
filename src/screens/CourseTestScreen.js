f43e36aee9f5e48ee533e0ef1e77457de2c7eb39
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-19 17:28:42 +0600

    Created and initialized CourseTestScreen. #89

---
+++ src/screens/CourseTestScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CourseTestScreen = () => {
+  return (
+    <View>
+      <Text>CourseTestScreen</Text>
+    </View>
+  )
+}
+
+export default CourseTestScreen
