be3e8ecb55fa8edb858234692e7ccd18c6acf668
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-22 17:14:28 +0600

    Initialized screen. #92

---
+++ src/screens/CourseTaskScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CourseTaskScreen = () => {
+  return (
+    <View>
+      <Text>CourseTaskScreen</Text>
+    </View>
+  )
+}
+
+export default CourseTaskScreen
