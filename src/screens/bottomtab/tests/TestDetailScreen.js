74b52e03f9dc054342a750208750f06e0e791ed4
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-24 14:46:11 +0600

    Initialized Test Detail Screen. #64

---
+++ src/screens/bottomtab/tests/TestDetailScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TestDetailScreen = () => {
+  return (
+    <View>
+      <Text>TestDetailScreen</Text>
+    </View>
+  )
+}
+
+export default TestDetailScreen
