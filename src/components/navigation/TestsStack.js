f0fae49ae3db84d73e9a52b25f6f70ffedbb1d93
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-24 14:45:47 +0600

    Done TestsStack. #64

--- src/components/navigation/TestsStack.js
+++ src/components/navigation/TestsStack.js
@@ -1,14 +1,53 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { strings } from '../../localization'
+import { ROUTE_NAMES } from './routes'
+import TestsScreen from '../../screens/bottomtab/tests/TestsScreen'
+import TestDetailScreen from '../../screens/bottomtab/tests/TestDetailScreen'
+import { useSettings } from '../context/Provider'
 
+const TestsStack = createNativeStackNavigator()
 
+const Tests = () => {
 
-const TestsStack = () => {
-  return (
-    <View>
-      <Text>TestsStack</Text>
-    </View>
-  )
+    const TITLE = strings.Тесты
+
+    const screens = [
+        {
+            name: ROUTE_NAMES.tests,
+            component: TestsScreen
+        },
+        {
+            name: ROUTE_NAMES.testDetail,
+            component: TestDetailScreen
+        },
+    ]
+
+    const { settings } = useSettings()
+
+    return (
+        <TestsStack.Navigator>
+            {
+                screens.map((screen, index) => (
+                    <TestsStack.Screen 
+                        name={screen.name} 
+                        key={index} 
+                        component={screen.component}
+                        options={
+                            screen.name === ROUTE_NAMES.tests
+                            ?
+                            navHeaderOptions(settings?.logo, TITLE)
+                            :
+                            {
+                                headerTitle: strings.Тесты,
+                                headerBackTitleVisible: false
+                            }
+                        }
+                    />        
+                ))
+            }
+        </TestsStack.Navigator>
+    )
 }
 
-export default TestsStack
+export default Tests
