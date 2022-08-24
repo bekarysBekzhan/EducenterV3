d6338786b8d2f97c3e4d995cb23d4cc394a94dbf
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-24 14:25:14 +0600

    Initialized tests stack. #64

---
+++ src/components/navigation/TestsStack.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+
+const TestsStack = () => {
+  return (
+    <View>
+      <Text>TestsStack</Text>
+    </View>
+  )
+}
+
+export default TestsStack
