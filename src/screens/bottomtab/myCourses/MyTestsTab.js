24554612596795af01ceaa480889485bfbabea1a
YeldosQoja <yeldos.kozha@gmail.com>
2022-08-26 14:00:44 +0600

    Added ModuleMyTestItem component. #63

--- src/screens/bottomtab/myCourses/MyTestsTab.js
+++ src/screens/bottomtab/myCourses/MyTestsTab.js
@@ -1,94 +1,80 @@
-import { View, Text, StyleSheet, FlatList, ActivityIndicator } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import UniversalView from '../../../components/view/UniversalView'
-import { useFetching } from '../../../hooks/useFetching'
-import LoadingScreen from '../../../components/LoadingScreen'
-import ModuleTestItem from '../../../components/test/ModuleTestItem'
-import { MyCourseService } from '../../../services/API'
-import { APP_COLORS, WIDTH } from '../../../constans/constants'
-import { ROUTE_NAMES } from '../../../components/navigation/routes'
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import UniversalView from '../../../components/view/UniversalView';
+import {useFetching} from '../../../hooks/useFetching';
+import LoadingScreen from '../../../components/LoadingScreen';
+import {MyCourseService} from '../../../services/API';
+import {APP_COLORS, WIDTH} from '../../../constans/constants';
+import {ROUTE_NAMES} from '../../../components/navigation/routes';
+import { TimeIcon } from '../../../assets/icons';
+import RowView from '../../../components/view/RowView';
+import { setFontStyle } from '../../../utils/utils';
+import { strings } from '../../../localization';
 
-const MyTestsTab = (props) => {
-
-  const [data, setData] = useState(null)
-  const [page, setPage] = useState(1)
-  const [lastPage, setLastPage] = useState(1)
-  const [fetchTests, isFetching, fetchingError] = useFetching(async() => {
-    const response = await MyCourseService.fetchMyTests()
-    setData(response.data?.data)
-    setLastPage(response.data?.last_page)
-  })
-  const [fetchNext, isFetchingNext, fetchingNextError] = useFetching(async() => {
-    const response = await MyCourseService.fetchMyTests("", page)
-    setData(prev => prev.concat(response.data?.data))
-  })
+const MyTestsTab = props => {
+  const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+  const [fetchTests, isFetching, fetchingError] = useFetching(async () => {
+    const response = await MyCourseService.fetchMyTests();
+    setData(response.data?.data);
+    setLastPage(response.data?.last_page);
+  });
+  const [fetchNext, isFetchingNext, fetchingNextError] = useFetching(
+    async () => {
+      const response = await MyCourseService.fetchMyTests('', page);
+      setData(prev => prev.concat(response.data?.data));
+    },
+  );
 
   // fetchTest error handler
   useEffect(() => {
-    if(fetchingError) {
-      console.log(fetchingError)
+    if (fetchingError) {
+      console.log(fetchingError);
     }
-  }, [fetchingError])
+  }, [fetchingError]);
 
   // fetchNext error handler
   useEffect(() => {
-    if(fetchingNextError) {
-      console.log(fetchingNextError)
+    if (fetchingNextError) {
+      console.log(fetchingNextError);
     }
-  })
+  });
 
   useEffect(() => {
-    if(page === 1) {
-      fetchTests()
+    if (page === 1) {
+      fetchTests();
     } else {
-      fetchNext()
+      fetchNext();
     }
-  }, [page])
+  }, [page]);
 
+  const renderTest = ({item, index}) => {
+    return <View/>;
 
-  const testItemTapped = (testID) => {
-    console.log("test : " , testID)
-    props.navigation.navigate(ROUTE_NAMES.myTestDetail, {id: testID})
-  }
-
-  const renderTest = ({ item, index }) => {
-    return(
-      <ModuleTestItem  
-        id={item?.id}
-        index={index}
-        categoryName={item?.category?.name}
-        time={item?.timer}
-        title={item?.title}
-        attempts={item?.attempts}
-        price={item?.price}
-        oldPrice={item?.old_price}
-        onPress={testItemTapped}
-      />
-    )
-  }
+  };
 
   const renderFooter = () => (
-    <View
-      style={styles.footer}
-    >
-      {
-        isFetchingNext
-        ?
-        <ActivityIndicator color={APP_COLORS.primary}/>
-        :
-        null
-      }
+    <View style={styles.footer}>
+      {isFetchingNext ? <ActivityIndicator color={APP_COLORS.primary} /> : null}
     </View>
-  )
+  );
 
   const onEndReached = () => {
     if (page < lastPage && !isFetchingNext) {
-      setPage(prev => prev + 1)
+      setPage(prev => prev + 1);
     }
-  }
+  };
 
   if (isFetching) {
-    return <LoadingScreen/>
+    return <LoadingScreen />;
   }
 
   return (
@@ -103,26 +89,101 @@ const MyTestsTab = (props) => {
         onEndReached={onEndReached}
         refreshing={isFetching}
         onRefresh={() => {
-          if(page === 1) {
-            fetchTests()
+          if (page === 1) {
+            fetchTests();
           }
-          setPage(1)
+          setPage(1);
         }}
       />
     </UniversalView>
-  )
-}
+  );
+};
+
+const ModuleMyTestItem = ({
+  id,
+  index,
+  categoryName,
+  time,
+  title,
+  attempts,
+  onPress = () => undefined,
+}) => {
+  const testItemTapped = () => {
+    console.log('test : ', id);
+    props.navigation.navigate(ROUTE_NAMES.myTestDetail, {id});
+  };
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.9}
+      onPress={testItemTapped}
+      style={item.container}>
+      <RowView style={styles.row1}>
+        <Text style={styles.title}>{title}</Text>
+        <RowView>
+          <TimeIcon color={APP_COLORS.placeholder} size={16} />
+          <Text style={styles.time}>
+            {time ? time : 30} {strings.мин}
+          </Text>
+        </RowView>
+      </RowView>
+      <Text></Text>
+      {
+        
+      }
+
+
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    padding: 16
+    padding: 16,
   },
   footer: {
     width: WIDTH - 32,
     height: 30,
-    justifyContent: "center",
-    alignItems: 'center'
-  }
-})
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-export default MyTestsTab
+const item = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 12,
+    borderBottomWidth: 0.45,
+    borderColor: APP_COLORS.border,
+  },
+  row1: {
+    justifyContent: 'space-between',
+  },
+  category: {
+    ...setFontStyle(11, '600', APP_COLORS.placeholder),
+    textTransform: 'uppercase',
+  },
+  time: {
+    ...setFontStyle(10, '600', APP_COLORS.placeholder),
+    marginLeft: 4,
+  },
+  title: {
+    ...setFontStyle(17, '600'),
+    marginVertical: 5,
+  },
+  attempts: {
+    ...setFontStyle(12, '500', APP_COLORS.placeholder),
+  },
+  row2: {
+    justifyContent: 'space-between',
+  },
+  button: {
+    marginTop: 5,
+  },
+  buttonText: {
+    ...setFontStyle(14, '600', APP_COLORS.primary),
+    textTransform: 'uppercase',
+  },
+});
+
+export default MyTestsTab;
