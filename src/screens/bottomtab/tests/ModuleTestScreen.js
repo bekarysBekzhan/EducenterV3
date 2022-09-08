f7f249abbac27cc73a2623346554b05803898e17
YeldosQoja <yeldos.kozha@gmail.com>
2022-09-08 20:12:10 +0600

    Replaced ModuleTestScreen with TestCompletedScreen. #108

--- src/screens/bottomtab/tests/ModuleTestScreen.js
+++ src/screens/bottomtab/tests/ModuleTestScreen.js
@@ -32,7 +32,7 @@ const ModuleTestScreen = props => {
     console.log("Test id : " , data?.id)
     const response = await TestService.finishTest(data?.id)
     const finishedTestData = response.data?.data
-    props.navigation.navigate(ROUTE_NAMES.testCompleted, { passed: finishedTestData?.passed, correct: finishedTestData?.score, total: finishedTestData?.tests_count, id: data?.id })
+    props.navigation.replace(ROUTE_NAMES.testCompleted, { passed: finishedTestData?.passed, correct: finishedTestData?.score, total: finishedTestData?.tests_count, id: data?.id })
   })
 
   useEffect(() => {
