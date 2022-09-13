b00ab5dc9d2971723884a6d37080f6d353fcd23d
YeldosQoja <yeldos.kozha@gmail.com>
2022-09-13 16:19:46 +0600

    Created SelectSubjectsScreen. #117

---
+++ src/screens/ubt/SelectSubjectsScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import UniversalView from '../../components/view/UniversalView'
+
+const SelectSubjectsScreen = (props) => {
+  return (
+    <UniversalView>
+        
+    </UniversalView>
+  )
+}
+
+export default SelectSubjectsScreen
