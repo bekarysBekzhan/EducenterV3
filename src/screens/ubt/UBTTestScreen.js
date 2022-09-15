dbc9353687c18fed57bca872ed146576915d4883
YeldosQoja <yeldos.kozha@gmail.com>
2022-09-15 20:15:27 +0600

    Created UBTTestScreen. #117

---
+++ src/screens/ubt/UBTTestScreen.js
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import UniversalView from '../../components/view/UniversalView'
+
+const UBTTestScreen = (props) => {
+  return (
+    <UniversalView style={styles.container}>
+
+    </UniversalView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
+
+export default UBTTestScreen
