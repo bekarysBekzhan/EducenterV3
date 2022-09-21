9d474aecc5b8d80d173aeebf2ecb53980b5465a7
YeldosQoja <yeldos.kozha@gmail.com>
2022-09-21 19:01:17 +0600

    Changed name and bundle id. #117

--- android/app/src/main/java/com/buginsoft/educenter/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/buginsoft/educenter/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.educenter.newarchitecture;
+package com.buginsoft.educenter.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.EmptyReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.educenter.BuildConfig;
-import com.educenter.newarchitecture.components.MainComponentsRegistry;
-import com.educenter.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.buginsoft.educenter.BuildConfig;
+import com.buginsoft.educenter.newarchitecture.components.MainComponentsRegistry;
+import com.buginsoft.educenter.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
