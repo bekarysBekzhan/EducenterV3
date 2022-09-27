9f354ef49643f469460e28cc92adc7f4645128ce
YeldosQoja <yeldos.kozha@gmail.com>
2022-09-27 18:23:44 +0600

    Added getInitialNotification method to check for message the app is caused to open by. #119

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Provider} from './src/components/context/Provider';
 import Navigation from './src/components/navigation/MainStack';
@@ -10,6 +10,9 @@ import TrackPlayer, {
 } from 'react-native-track-player';
 import { firebaseService } from './src/services/FirebaseService';
 import { LocalNotificationService } from './src/services/LocalNotificationService';
+import { NOTIFICATION_TYPE } from './src/constans/constants';
+import { navigate } from './src/components/navigation/RootNavigation';
+import { ROUTE_NAMES } from './src/components/navigation/routes';
 
 const events = [
   Event.PlaybackError,
@@ -21,12 +24,26 @@ const events = [
 
 const App = () => {
 
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     // Initialize player
     initPlayer();
     // Firebase Messaging Service
     firebaseService.register()
     firebaseService.registerAppWithFCM()
+    firebaseService.getInitialNotification().then(remoteMessage => {
+      if (remoteMessage) {
+        console.log('Notification caused app to open from quit state:', remoteMessage);
+        const notificationData = remoteMessage?.data
+        if (notificationData?.type === NOTIFICATION_TYPE.news) {
+          navigate(ROUTE_NAMES.newsDetail, { newsId: notificationData?.id })
+        } else {
+          navigate(ROUTE_NAMES.bottomTab)
+        }
+      }
+      setLoading(false)
+    })
     return () => {
       deinitPlayer();
       firebaseService.unsubscribe()
@@ -49,6 +66,10 @@ const App = () => {
   useTrackPlayerEvents(events, () => {
     console.log();
   });
+
+  if (loading) {
+    return null
+  }
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
