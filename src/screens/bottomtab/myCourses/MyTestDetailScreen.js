764930d2fa2ae34bd7027e87076f83b0abc375d9
YeldosQoja <yeldos.kozha@gmail.com>
2022-10-17 17:47:51 +0600

    Added onNavigation action.

--- src/screens/bottomtab/myCourses/MyTestDetailScreen.js
+++ src/screens/bottomtab/myCourses/MyTestDetailScreen.js
@@ -1,35 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import UniversalView from '../../../components/view/UniversalView'
-import DetailView from '../../../components/view/DetailView'
-import { useFetching } from '../../../hooks/useFetching'
-import { TestService } from '../../../services/API'
-import LoadingScreen from '../../../components/LoadingScreen'
-import Person from '../../../components/Person'
-import { strings } from '../../../localization'
-import TransactionButton from '../../../components/button/TransactionButton'
+import React, {useEffect, useState} from 'react';
+import UniversalView from '../../../components/view/UniversalView';
+import DetailView from '../../../components/view/DetailView';
+import {useFetching} from '../../../hooks/useFetching';
+import {TestService} from '../../../services/API';
+import LoadingScreen from '../../../components/LoadingScreen';
+import Person from '../../../components/Person';
+import {strings} from '../../../localization';
+import TransactionButton from '../../../components/button/TransactionButton';
+import {ROUTE_NAMES} from '../../../components/navigation/routes';
 
-const MyTestDetailScreen = (props) => {
+const MyTestDetailScreen = props => {
+  const id = props.route?.params?.id;
 
-  const id = props.route?.params?.id
-
-  const [data, setData] = useState(null)
-  const [fetchTest, isFetching, fetchingError] = useFetching(async() => {
-    const response = await TestService.fetchTestByID(id)
-    setData(response.data?.data)
-  })
-
-  useEffect(() => {
-    fetchTest()
-  }, [])
+  const [data, setData] = useState(null);
+  const [fetchTest, isFetching, fetchingError] = useFetching(async () => {
+    const response = await TestService.fetchTestByID(id);
+    setData(response.data?.data);
+  });
 
   useEffect(() => {
-    if(fetchingError) {
-      console.log(fetchingError)
+    fetchTest();
+  }, []);
+
+  useEffect(() => {
+    if (fetchingError) {
+      console.log(fetchingError);
     }
-  }, [fetchingError])
+  }, [fetchingError]);
+
+  const onNavigation = () => {
+    props.navigation.navigate(ROUTE_NAMES.testPreview, {
+      id: data?.id,
+      title: data?.title,
+      type: 'module',
+    });
+  };
 
   if (isFetching) {
-    return <LoadingScreen/>
+    return <LoadingScreen />;
   }
 
   return (
@@ -43,22 +51,23 @@ const MyTestDetailScreen = (props) => {
           description={data?.description}
         />
         <Person
-          name={(data?.author?.name ? data?.author?.name : "") + " " + (data?.author?.surname ? data?.author?.surname : "")}
+          name={
+            (data?.author?.name ? data?.author?.name : '') +
+            ' ' +
+            (data?.author?.surname ? data?.author?.surname : '')
+          }
           image={data?.author?.avatar}
           status={strings['Автор теста']}
           description={data?.author?.description}
           extraStyles={{
             margin: 16,
-            marginTop: 32
+            marginTop: 32,
           }}
         />
       </UniversalView>
-      <TransactionButton
-        text={strings['Пройти тест']}
-        onPress={() => undefined}
-      />
+      <TransactionButton text={strings['Пройти тест']} onPress={onNavigation} />
     </UniversalView>
-  )
-}
+  );
+};
 
-export default MyTestDetailScreen
+export default MyTestDetailScreen;
