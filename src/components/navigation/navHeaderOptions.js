ac212f870ec904b7c6aad04d2ae56ca69fef0c05
Ruslan-BG <praktika.95@gmail.com>
2022-10-18 14:18:57 +0600

    fix navHeader

--- src/components/navigation/navHeaderOptions.js
+++ src/components/navigation/navHeaderOptions.js
@@ -7,7 +7,7 @@ export const navHeaderOptions = (image, title) => {
   return {
     headerLeft: () => <FastImage source={{uri: image}} style={styles.logo} />,
     headerTitle: title,
-    headerTitleAlign: 'left',
+    headerTitleAlign: 'center',
     headerTitleStyle: styles.navigationTitle,
     headerLeftContainerStyle: styles.navigationHeader,
   };
