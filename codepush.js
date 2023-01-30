74327b24c567ff5c1e4aa20be70ac771eae3fe6e
ekosh02 <turapbaeveldos@gmail.com>
2023-01-30 12:18:23 +0600

    added codepush script

---
+++ codepush.js
@@ -0,0 +1,37 @@
+const {exec} = require('child_process');
+
+const cudePushRun = () => {
+  let codePushName = 'Educenter';
+  let android = '-Android';
+  let ios = '-IOS';
+  let bashCommand = '';
+
+  const readline = require('readline-sync');
+
+  let chooseOS = readline.question('1. iOS\n2. Android\n3. all\n');
+
+  if (chooseOS == 1) {
+    bashCommand = `appcenter codepush release-react -a Educenter/${codePushName}${ios} -d Production`;
+    console.log(bashCommand);
+  } else if (chooseOS == 2) {
+    bashCommand = `appcenter codepush release-react -a Educenter/${codePushName}${android} -d Production`;
+    console.log(bashCommand);
+  } else if (chooseOS == 3) {
+    bashCommand = `appcenter codepush release-react -a Educenter/${codePushName}${ios} -d Production && appcenter codepush release-react -a Educenter/${codePushName}${android} -d Production`;
+    console.log(bashCommand);
+  } else {
+    console.log('ERROR');
+    return;
+  }
+
+  exec(bashCommand, (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('STDOUT: \n' + stdout);
+    console.log('STDERR: \n' + stderr);
+  });
+};
+
+cudePushRun();
